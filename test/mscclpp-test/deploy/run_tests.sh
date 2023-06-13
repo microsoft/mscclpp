6ee4e803178b628c7b80683d68ceac1712ceaa8b
Binyang2014 <binyli@microsoft.com>
2023-06-13 14:34:07 +0800

    Create Azure pipeline for multi-node tests (#97)
    Create Azure pipeline to run mscclpp-test on multi-nodes

---
+++ test/mscclpp-test/deploy/run_tests.sh
@@ -0,0 +1,16 @@
+set -e
+
+echo "=================Run allgather_test_perf on 2 nodes========================="
+/usr/local/mpi/bin/mpirun --allow-run-as-root -np 16 --bind-to numa -hostfile /root/mscclpp/hostfile_mpi \
+  -x MSCCLPP_DEBUG=WARN -x LD_LIBRARY_PATH=/root/mscclpp/build:$LD_LIBRARY_PATH \
+  -npernode 8 /root/mscclpp/build/test/mscclpp-test/allgather_test_perf -b 1K -e 1G -f 2 -k 0
+
+# For kernel 2, the message size must can be divided by 3
+/usr/local/mpi/bin/mpirun --allow-run-as-root -np 16 --bind-to numa -hostfile /root/mscclpp/hostfile_mpi \
+  -x MSCCLPP_DEBUG=WARN -x LD_LIBRARY_PATH=/root/mscclpp/build:$LD_LIBRARY_PATH \
+  -npernode 8 /root/mscclpp/build/test/mscclpp-test/allgather_test_perf -b 3K -e 3G -f 2 -k 2
+
+echo "==================Run alltoall_test_perf on 2 nodes========================="
+/usr/local/mpi/bin/mpirun --allow-run-as-root -np 16 --bind-to numa -hostfile /root/mscclpp/hostfile_mpi \
+  -x MSCCLPP_DEBUG=WARN -x LD_LIBRARY_PATH=/root/mscclpp/build:$LD_LIBRARY_PATH \
+  -npernode 8 /root/mscclpp/build/test/mscclpp-test/alltoall_test_perf -b 1K -e 1G -f 2 -k 0
