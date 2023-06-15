6cd8960394e14965fc9abcd702564fd2bdbac970
Changho Hwang <changhohwang@microsoft.com>
2023-06-15 20:55:57 +0800

    DirectChannel Unit Tests (#102)
    * Add DirectChannel unit tests
    * Split mp_unit_tests.cu into multiple files

---
+++ test/mp_unit/device_channel_tests.cu
@@ -0,0 +1,138 @@
+#include "mp_unit_tests.hpp"
+
+void DeviceChannelOneToOneTest::SetUp() {
+  // Use only two ranks
+  setNumRanksToUse(2);
+  CommunicatorTestBase::SetUp();
+  channelService = std::make_shared<mscclpp::channel::DeviceChannelService>(*communicator.get());
+}
+
+void DeviceChannelOneToOneTest::TearDown() { CommunicatorTestBase::TearDown(); }
+
+void DeviceChannelOneToOneTest::setupMeshConnections(std::vector<mscclpp::channel::SimpleDeviceChannel>& devChannels,
+                                                     bool useIbOnly, void* sendBuff, size_t sendBuffBytes,
+                                                     void* recvBuff, size_t recvBuffBytes) {
+  const int rank = communicator->bootstrapper()->getRank();
+  const int worldSize = communicator->bootstrapper()->getNranks();
+  const bool isInPlace = (recvBuff == nullptr);
+  mscclpp::TransportFlags transport = mscclpp::Transport::CudaIpc | ibTransport;
+
+  connectMesh(useIbOnly);
+
+  for (int r = 0; r < worldSize; r++) {
+    if (r == rank) {
+      continue;
+    }
+    mscclpp::RegisteredMemory sendMemory;
+    mscclpp::RegisteredMemory remoteMemory;
+
+    if (isInPlace) {
+      registerMemoryPair(sendBuff, sendBuffBytes, transport, 0, r, sendMemory, remoteMemory);
+    } else {
+      sendMemory = communicator->registerMemory(recvBuff, recvBuffBytes, transport);
+      mscclpp::RegisteredMemory recvMemory;
+      registerMemoryPair(recvBuff, recvBuffBytes, transport, 0, r, recvMemory, remoteMemory);
+    }
+
+    mscclpp::channel::ChannelId cid = channelService->addChannel(connections[r]);
+    communicator->setup();
+
+    devChannels.emplace_back(channelService->deviceChannel(cid), channelService->addMemory(remoteMemory),
+                             channelService->addMemory(sendMemory));
+  }
+}
+
+__constant__ mscclpp::channel::SimpleDeviceChannel gChannelOneToOneTestConstDevChans;
+
+__global__ void kernelDevicePingPong(int* buff, int rank, int nElem, int* ret) {
+  mscclpp::channel::SimpleDeviceChannel& devChan = gChannelOneToOneTestConstDevChans;
+  volatile int* sendBuff = (volatile int*)buff;
+  int nTries = 1000;
+  int flusher = 0;
+  int rank1Offset = 10000000;
+  for (int i = 0; i < nTries; i++) {
+    if (rank == 0) {
+      if (i > 0) {
+        if (threadIdx.x == 0) devChan.wait();
+        __syncthreads();
+        for (int j = threadIdx.x; j < nElem; j += blockDim.x) {
+          if (sendBuff[j] != rank1Offset + i - 1 + j) {
+            // printf("rank 0 ERROR: sendBuff[%d] = %d, expected %d\n", j, sendBuff[j], rank1Offset + i - 1 + j);
+            *ret = 1;
+            break;
+          }
+        }
+      }
+      for (int j = threadIdx.x; j < nElem; j += blockDim.x) {
+        sendBuff[j] = i + j;
+      }
+      __syncthreads();
+      // __threadfence_system(); // not necessary if we make sendBuff volatile
+      if (threadIdx.x == 0) devChan.putWithSignal(0, nElem * sizeof(int));
+    }
+    if (rank == 1) {
+      if (threadIdx.x == 0) devChan.wait();
+      __syncthreads();
+      for (int j = threadIdx.x; j < nElem; j += blockDim.x) {
+        if (sendBuff[j] != i + j) {
+          // printf("rank 1 ERROR: sendBuff[%d] = %d, expected %d\n", j, sendBuff[j], i + j);
+          *ret = 1;
+          break;
+        }
+      }
+      if (i < nTries - 1) {
+        for (int j = threadIdx.x; j < nElem; j += blockDim.x) {
+          sendBuff[j] = rank1Offset + i + j;
+        }
+        __syncthreads();
+        // __threadfence_system(); // not necessary if we make sendBuff volatile
+        if (threadIdx.x == 0) devChan.putWithSignal(0, nElem * sizeof(int));
+      }
+    }
+    flusher++;
+    if (flusher == 100) {
+      devChan.flush();
+      flusher = 0;
+    }
+  }
+}
+
+TEST_F(DeviceChannelOneToOneTest, PingPongIb) {
+  if (gEnv->rank >= numRanksToUse) return;
+
+  const int nElem = 4 * 1024 * 1024;
+
+  std::vector<mscclpp::channel::SimpleDeviceChannel> devChannels;
+  std::shared_ptr<int> buff = mscclpp::allocSharedCuda<int>(nElem);
+  setupMeshConnections(devChannels, true, buff.get(), nElem * sizeof(int));
+
+  ASSERT_EQ(devChannels.size(), 1);
+  MSCCLPP_CUDATHROW(cudaMemcpyToSymbol(gChannelOneToOneTestConstDevChans, devChannels.data(),
+                                       sizeof(mscclpp::channel::SimpleDeviceChannel)));
+
+  channelService->startProxy();
+
+  std::shared_ptr<int> ret = mscclpp::makeSharedCudaHost<int>(0);
+
+  kernelDevicePingPong<<<1, 1024>>>(buff.get(), gEnv->rank, 1, ret.get());
+  MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
+
+  EXPECT_EQ(*ret, 0);
+
+  kernelDevicePingPong<<<1, 1024>>>(buff.get(), gEnv->rank, 1024, ret.get());
+  MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
+
+  EXPECT_EQ(*ret, 0);
+
+  kernelDevicePingPong<<<1, 1024>>>(buff.get(), gEnv->rank, 1024 * 1024, ret.get());
+  MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
+
+  EXPECT_EQ(*ret, 0);
+
+  kernelDevicePingPong<<<1, 1024>>>(buff.get(), gEnv->rank, 4 * 1024 * 1024, ret.get());
+  MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
+
+  EXPECT_EQ(*ret, 0);
+
+  channelService->stopProxy();
+}
---
+++ test/mp_unit/mp_unit_tests.cc
@@ -0,0 +1,83 @@
+#include "mp_unit_tests.hpp"
+
+#include <mpi.h>
+
+#include <sstream>
+
+const char gDefaultIpPort[] = "127.0.0.1:50053";
+MultiProcessTestEnv* gEnv = nullptr;
+
+mscclpp::Transport ibIdToTransport(int id) {
+  mscclpp::Transport IBs[] = {mscclpp::Transport::IB0, mscclpp::Transport::IB1, mscclpp::Transport::IB2,
+                              mscclpp::Transport::IB3, mscclpp::Transport::IB4, mscclpp::Transport::IB5,
+                              mscclpp::Transport::IB6, mscclpp::Transport::IB7};
+  return IBs[id];
+}
+
+MultiProcessTestEnv::MultiProcessTestEnv(int argc, const char** argv) : argc(argc), argv(argv) {}
+
+static std::unordered_map<std::string, std::string> parseArgs(int argc, const char* argv[]) {
+  auto printUsage = [](const char* prog) {
+    std::stringstream ss;
+    ss << "Usage: " << prog << " [-ip_port IP:PORT]\n";
+    std::cout << ss.str();
+  };
+
+  std::unordered_map<std::string, std::string> options;
+
+  // Default values
+  options["ip_port"] = gDefaultIpPort;
+
+  // Parse the command line arguments
+  for (int i = 1; i < argc; i++) {
+    std::string arg = argv[i];
+    if (arg == "-ip_port") {
+      if (i + 1 < argc) {
+        options["ip_port"] = argv[++i];
+      } else {
+        throw std::invalid_argument("Error: -ip_port option requires an argument.\n");
+      }
+    } else if (arg == "-help" || arg == "-h") {
+      printUsage(argv[0]);
+      exit(0);
+    } else {
+      throw std::invalid_argument("Error: Unknown option " + std::string(argv[i]) + "\n");
+    }
+  }
+  return options;
+}
+
+void MultiProcessTestEnv::SetUp() {
+  MPI_Init(NULL, NULL);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+  // get the local number of nodes with MPI
+  MPI_Comm shmcomm;
+  MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &shmcomm);
+  int shmrank;
+  MPI_Comm_size(shmcomm, &shmrank);
+  nRanksPerNode = shmrank;
+  MPI_Comm_free(&shmcomm);
+
+  // parse the command line arguments
+  args = parseArgs(argc, argv);
+}
+
+void MultiProcessTestEnv::TearDown() { MPI_Finalize(); }
+
+void MultiProcessTest::TearDown() {
+  // Wait for all ranks to finish the previous test
+  MPI_Barrier(MPI_COMM_WORLD);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  gEnv = new MultiProcessTestEnv(argc, (const char**)argv);
+  ::testing::AddGlobalTestEnvironment(gEnv);
+  return RUN_ALL_TESTS();
+}
+
+TEST_F(MultiProcessTest, Prelim) {
+  // Test to make sure the MPI environment is set up correctly
+  ASSERT_GE(gEnv->worldSize, 2);
+}
