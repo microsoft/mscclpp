f424f15db6ac1a376e5f2c0c57b217b82e8ef77a
Changho Hwang <changhohwang@microsoft.com>
2023-07-11 07:41:47 +0000

    Lint

--- python/core_py.cpp
+++ python/core_py.cpp
@@ -3,110 +3,129 @@
 
 #include <nanobind/nanobind.h>
 #include <nanobind/operators.h>
-#include <nanobind/stl/string.h>
 #include <nanobind/stl/shared_ptr.h>
+#include <nanobind/stl/string.h>
+
 #include <mscclpp/core.hpp>
 
 namespace nb = nanobind;
 using namespace mscclpp;
 
-template<typename T>
-void def_nonblocking_future(nb::handle &m, const std::string &typestr) {
-    std::string pyclass_name = std::string("NonblockingFuture") + typestr;
-    nb::class_<NonblockingFuture<T>>(m, pyclass_name.c_str())
+template <typename T>
+void def_nonblocking_future(nb::handle& m, const std::string& typestr) {
+  std::string pyclass_name = std::string("NonblockingFuture") + typestr;
+  nb::class_<NonblockingFuture<T>>(m, pyclass_name.c_str())
       .def("ready", &NonblockingFuture<T>::ready)
       .def("get", &NonblockingFuture<T>::get);
 }
 
 NB_MODULE(mscclpp, m) {
   nb::class_<Bootstrap>(m, "Bootstrap")
-    .def("get_rank", &Bootstrap::getRank)
-    .def("get_n_ranks", &Bootstrap::getNranks)
-    .def("send", [](Bootstrap* self, uintptr_t ptr, size_t size, int peer, int tag) {
-      void* data = reinterpret_cast<void*>(ptr);
-      self->send(data, size, peer, tag);
-    }, nb::arg("data"), nb::arg("size"), nb::arg("peer"), nb::arg("tag"))
-    .def("recv", [](Bootstrap* self, uintptr_t ptr, size_t size, int peer, int tag) {
-      void* data = reinterpret_cast<void*>(ptr);
-      self->recv(data, size, peer, tag);
-    }, nb::arg("data"), nb::arg("size"), nb::arg("peer"), nb::arg("tag"))
-    .def("all_gather", &Bootstrap::allGather, nb::arg("allData"), nb::arg("size"))
-    .def("barrier", &Bootstrap::barrier)
-    .def("send", (void (Bootstrap::*)(const std::vector<char>&, int, int)) &Bootstrap::send, nb::arg("data"), nb::arg("peer"), nb::arg("tag"))
-    .def("recv", (void (Bootstrap::*)(std::vector<char>&, int, int)) &Bootstrap::recv, nb::arg("data"), nb::arg("peer"), nb::arg("tag"));
+      .def("get_rank", &Bootstrap::getRank)
+      .def("get_n_ranks", &Bootstrap::getNranks)
+      .def(
+          "send",
+          [](Bootstrap* self, uintptr_t ptr, size_t size, int peer, int tag) {
+            void* data = reinterpret_cast<void*>(ptr);
+            self->send(data, size, peer, tag);
+          },
+          nb::arg("data"), nb::arg("size"), nb::arg("peer"), nb::arg("tag"))
+      .def(
+          "recv",
+          [](Bootstrap* self, uintptr_t ptr, size_t size, int peer, int tag) {
+            void* data = reinterpret_cast<void*>(ptr);
+            self->recv(data, size, peer, tag);
+          },
+          nb::arg("data"), nb::arg("size"), nb::arg("peer"), nb::arg("tag"))
+      .def("all_gather", &Bootstrap::allGather, nb::arg("allData"), nb::arg("size"))
+      .def("barrier", &Bootstrap::barrier)
+      .def("send", (void(Bootstrap::*)(const std::vector<char>&, int, int)) & Bootstrap::send, nb::arg("data"),
+           nb::arg("peer"), nb::arg("tag"))
+      .def("recv", (void(Bootstrap::*)(std::vector<char>&, int, int)) & Bootstrap::recv, nb::arg("data"),
+           nb::arg("peer"), nb::arg("tag"));
 
   nb::class_<UniqueId>(m, "UniqueId");
 
   nb::class_<TcpBootstrap, Bootstrap>(m, "TcpBootstrap")
-    .def(nb::init<int, int>(), "Do not use this constructor. Use create instead.")
-    .def_static("create", [](int rank, int nRanks) { return std::make_shared<TcpBootstrap>(rank, nRanks); }, nb::arg("rank"), nb::arg("nRanks"))
-    .def("create_unique_id", &TcpBootstrap::createUniqueId)
-    .def("get_unique_id", &TcpBootstrap::getUniqueId)
-    .def("initialize", (void (TcpBootstrap::*)(UniqueId)) &TcpBootstrap::initialize, nb::arg("uniqueId"))
-    .def("initialize", (void (TcpBootstrap::*)(const std::string&)) &TcpBootstrap::initialize, nb::arg("ifIpPortTrio"));
+      .def(nb::init<int, int>(), "Do not use this constructor. Use create instead.")
+      .def_static(
+          "create", [](int rank, int nRanks) { return std::make_shared<TcpBootstrap>(rank, nRanks); }, nb::arg("rank"),
+          nb::arg("nRanks"))
+      .def("create_unique_id", &TcpBootstrap::createUniqueId)
+      .def("get_unique_id", &TcpBootstrap::getUniqueId)
+      .def("initialize", (void(TcpBootstrap::*)(UniqueId)) & TcpBootstrap::initialize, nb::arg("uniqueId"))
+      .def("initialize", (void(TcpBootstrap::*)(const std::string&)) & TcpBootstrap::initialize,
+           nb::arg("ifIpPortTrio"));
 
   nb::enum_<Transport>(m, "Transport")
-    .value("Unknown", Transport::Unknown)
-    .value("CudaIpc", Transport::CudaIpc)
-    .value("IB0", Transport::IB0)
-    .value("IB1", Transport::IB1)
-    .value("IB2", Transport::IB2)
-    .value("IB3", Transport::IB3)
-    .value("IB4", Transport::IB4)
-    .value("IB5", Transport::IB5)
-    .value("IB6", Transport::IB6)
-    .value("IB7", Transport::IB7)
-    .value("NumTransports", Transport::NumTransports);
+      .value("Unknown", Transport::Unknown)
+      .value("CudaIpc", Transport::CudaIpc)
+      .value("IB0", Transport::IB0)
+      .value("IB1", Transport::IB1)
+      .value("IB2", Transport::IB2)
+      .value("IB3", Transport::IB3)
+      .value("IB4", Transport::IB4)
+      .value("IB5", Transport::IB5)
+      .value("IB6", Transport::IB6)
+      .value("IB7", Transport::IB7)
+      .value("NumTransports", Transport::NumTransports);
 
   nb::class_<TransportFlags>(m, "TransportFlags")
-    .def(nb::init<>())
-    .def(nb::init_implicit<Transport>(), nb::arg("transport"))
-    .def("has", &TransportFlags::has, nb::arg("transport"))
-    .def("none", &TransportFlags::none)
-    .def("any", &TransportFlags::any)
-    .def("all", &TransportFlags::all)
-    .def("count", &TransportFlags::count)
-    .def(nb::self |= nb::self)
-    .def(nb::self | nb::self)
-    .def(nb::self | Transport())
-    .def(nb::self &= nb::self)
-    .def(nb::self & nb::self)
-    .def(nb::self & Transport())
-    .def(nb::self ^= nb::self)
-    .def(nb::self ^ nb::self)
-    .def(nb::self ^ Transport())
-    .def(~nb::self)
-    .def(nb::self == nb::self)
-    .def(nb::self != nb::self);
+      .def(nb::init<>())
+      .def(nb::init_implicit<Transport>(), nb::arg("transport"))
+      .def("has", &TransportFlags::has, nb::arg("transport"))
+      .def("none", &TransportFlags::none)
+      .def("any", &TransportFlags::any)
+      .def("all", &TransportFlags::all)
+      .def("count", &TransportFlags::count)
+      .def(nb::self |= nb::self)
+      .def(nb::self | nb::self)
+      .def(nb::self | Transport())
+      .def(nb::self &= nb::self)
+      .def(nb::self & nb::self)
+      .def(nb::self & Transport())
+      .def(nb::self ^= nb::self)
+      .def(nb::self ^ nb::self)
+      .def(nb::self ^ Transport())
+      .def(~nb::self)
+      .def(nb::self == nb::self)
+      .def(nb::self != nb::self);
 
   nb::class_<RegisteredMemory>(m, "RegisteredMemory")
-    .def(nb::init<>())
-    .def("data", &RegisteredMemory::data)
-    .def("size", &RegisteredMemory::size)
-    .def("rank", &RegisteredMemory::rank)
-    .def("transports", &RegisteredMemory::transports)
-    .def("serialize", &RegisteredMemory::serialize)
-    .def_static("deserialize", &RegisteredMemory::deserialize, nb::arg("data"));
+      .def(nb::init<>())
+      .def("data", &RegisteredMemory::data)
+      .def("size", &RegisteredMemory::size)
+      .def("rank", &RegisteredMemory::rank)
+      .def("transports", &RegisteredMemory::transports)
+      .def("serialize", &RegisteredMemory::serialize)
+      .def_static("deserialize", &RegisteredMemory::deserialize, nb::arg("data"));
 
   nb::class_<Connection>(m, "Connection")
-    .def("write", &Connection::write, nb::arg("dst"), nb::arg("dstOffset"), nb::arg("src"), nb::arg("srcOffset"), nb::arg("size"))
-    .def("update_and_sync", &Connection::updateAndSync, nb::arg("dst"), nb::arg("dstOffset"), nb::arg("src"), nb::arg("newValue"))
-    .def("flush", &Connection::flush)
-    .def("remote_rank", &Connection::remoteRank)
-    .def("tag", &Connection::tag)
-    .def("transport", &Connection::transport)
-    .def("remote_transport", &Connection::remoteTransport);
+      .def("write", &Connection::write, nb::arg("dst"), nb::arg("dstOffset"), nb::arg("src"), nb::arg("srcOffset"),
+           nb::arg("size"))
+      .def("update_and_sync", &Connection::updateAndSync, nb::arg("dst"), nb::arg("dstOffset"), nb::arg("src"),
+           nb::arg("newValue"))
+      .def("flush", &Connection::flush)
+      .def("remote_rank", &Connection::remoteRank)
+      .def("tag", &Connection::tag)
+      .def("transport", &Connection::transport)
+      .def("remote_transport", &Connection::remoteTransport);
 
   def_nonblocking_future<RegisteredMemory>(m, "RegisteredMemory");
 
   nb::class_<Communicator>(m, "Communicator")
-    .def(nb::init<std::shared_ptr<Bootstrap>>(), nb::arg("bootstrap"))
-    .def("bootstrap", &Communicator::bootstrap)
-    .def("register_memory", [](Communicator* self, uintptr_t ptr, size_t size, TransportFlags transports) {
-      return self->registerMemory((void*)ptr, size, transports);
-    }, nb::arg("ptr"), nb::arg("size"), nb::arg("transports"))
-    .def("send_memory_on_setup", &Communicator::sendMemoryOnSetup, nb::arg("memory"), nb::arg("remoteRank"), nb::arg("tag"))
-    .def("recv_memory_on_setup", &Communicator::recvMemoryOnSetup, nb::arg("remoteRank"), nb::arg("tag"))
-    .def("connect_on_setup", &Communicator::connectOnSetup, nb::arg("remoteRank"), nb::arg("tag"), nb::arg("transport"))
-    .def("setup", &Communicator::setup);
+      .def(nb::init<std::shared_ptr<Bootstrap>>(), nb::arg("bootstrap"))
+      .def("bootstrap", &Communicator::bootstrap)
+      .def(
+          "register_memory",
+          [](Communicator* self, uintptr_t ptr, size_t size, TransportFlags transports) {
+            return self->registerMemory((void*)ptr, size, transports);
+          },
+          nb::arg("ptr"), nb::arg("size"), nb::arg("transports"))
+      .def("send_memory_on_setup", &Communicator::sendMemoryOnSetup, nb::arg("memory"), nb::arg("remoteRank"),
+           nb::arg("tag"))
+      .def("recv_memory_on_setup", &Communicator::recvMemoryOnSetup, nb::arg("remoteRank"), nb::arg("tag"))
+      .def("connect_on_setup", &Communicator::connectOnSetup, nb::arg("remoteRank"), nb::arg("tag"),
+           nb::arg("transport"))
+      .def("setup", &Communicator::setup);
 }
--- python/examples/send_recv.py
+++ python/examples/send_recv.py
@@ -6,10 +6,10 @@ import argparse
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument('if_ip_port_trio', type=str)
-parser.add_argument('-r', '--root', action='store_true')
-parser.add_argument('-n', '--num-elements', type=int, default=10)
-parser.add_argument('--gpu', action='store_true')
+parser.add_argument("if_ip_port_trio", type=str)
+parser.add_argument("-r", "--root", action="store_true")
+parser.add_argument("-n", "--num-elements", type=int, default=10)
+parser.add_argument("--gpu", action="store_true")
 args = parser.parse_args()
 
 if args.root:
@@ -24,15 +24,17 @@ comm = mscclpp.Communicator(boot)
 
 if args.gpu:
     import torch
-    print('Allocating GPU memory')
+
+    print("Allocating GPU memory")
     memory = torch.zeros(args.num_elements, dtype=torch.int32)
-    memory = memory.to('cuda')
+    memory = memory.to("cuda")
     ptr = memory.data_ptr()
     size = memory.numel() * memory.element_size()
 else:
     from array import array
-    print('Allocating host memory')
-    memory = array('i', [0] * args.num_elements)
+
+    print("Allocating host memory")
+    memory = array("i", [0] * args.num_elements)
     ptr, elements = memory.buffer_info()
     size = elements * memory.itemsize
 my_reg_mem = comm.register_memory(ptr, size, mscclpp.Transport.IB0)
@@ -54,14 +56,14 @@ if rank == 0:
     for i in range(args.num_elements):
         memory[i] = i + 1
     conn.write(other_reg_mem, 0, my_reg_mem, 0, size)
-    print('Done sending')
+    print("Done sending")
 else:
     while memory[0] == 0:
         time.sleep(0.1)
-    print('Received data')
+    print("Received data")
     for i in range(args.num_elements):
         if memory[i] != i + 1:
-            print(f'Mismatch at index {i}: expected {i + 1}, got {memory[i]}')
+            print(f"Mismatch at index {i}: expected {i + 1}, got {memory[i]}")
             break
     else:
-        print('All data matched expected values')
+        print("All data matched expected values")
