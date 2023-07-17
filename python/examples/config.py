4c6bb5181aa7f58227052d256bb4bcda0d375bc1
Binyang Li <binyli@microsoft.com>
2023-07-17 08:55:16 +0000

    lint

--- python/examples/config.py
+++ python/examples/config.py
@@ -1,10 +1,12 @@
 import mscclpp
 
+
 def main():
     config = mscclpp.Config.get_instance()
     config.set_bootstrap_connection_timeout_config(15)
     timeout = config.get_bootstrap_connection_timeout_config()
     assert timeout == 15
+
 
 if __name__ == "__main__":
     main()
