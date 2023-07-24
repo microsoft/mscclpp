5567fe5f474ff7780ec5ada6a44d631f96fd29d1
Olli Saarikivi <olsaarik@microsoft.com>
2023-07-24 23:42:17 +0000

    Format with black

--- python/mscclpp/__init__.py
+++ python/mscclpp/__init__.py
@@ -4,6 +4,7 @@
 from ._mscclpp import *
 import os as _os
 
+
 def get_include():
     """Return the directory that contains the MSCCL++ headers."""
-    return _os.path.join(_os.path.dirname(__file__), 'include')
+    return _os.path.join(_os.path.dirname(__file__), "include")
