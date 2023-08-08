5357391b04fe4f33ea86ea49f7f657b0ae56dac3
Changho Hwang <changhohwang@microsoft.com>
2023-08-08 11:27:52 +0000

    cmake fix

--- cmake/AddFormatTargets.cmake
+++ cmake/AddFormatTargets.cmake
@@ -10,7 +10,7 @@ find_program(CLANG_FORMAT clang-format)
 if(CLANG_FORMAT)
     message(STATUS "Found clang-format: ${CLANG_FORMAT}")
     set(FIND_DIRS ${PROJECT_SOURCE_DIR}/src ${PROJECT_SOURCE_DIR}/include ${PROJECT_SOURCE_DIR}/python ${PROJECT_SOURCE_DIR}/test)
-    add_custom_target(check-format-cpp
+    add_custom_target(check-format-cpp ALL
         COMMAND ${CLANG_FORMAT} -style=file --dry-run `find ${FIND_DIRS} -type f -name *.h -o -name *.hpp -o -name *.c -o -name *.cc -o -name *.cpp -o -name *.cu`
     )
     add_dependencies(check-format check-format-cpp)
