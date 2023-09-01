858e3818293067edbaa6be4ec1e66a382efba2ac
Binyang2014 <binyli@microsoft.com>
2023-09-01 21:22:11 +0800

    Pytest (#162)
    Port python tests to mscclpp.
    Please run
    `mpirun -tag-output -np 8 pytest ./python/test/test_mscclpp.py -x` to start pytest
    
    ---------
    
    Co-authored-by: Saeed Maleki <saemal@microsoft.com>
    Co-authored-by: Changho Hwang <changhohwang@microsoft.com>
    Co-authored-by: Saeed Maleki <30272783+saeedmaleki@users.noreply.github.com>

--- src/include/registered_memory.hpp
+++ src/include/registered_memory.hpp
@@ -35,12 +35,16 @@ struct RegisteredMemory::Impl {
   void* data;
   size_t size;
   int rank;
+  bool isRemote;
   uint64_t hostHash;
   TransportFlags transports;
   std::vector<TransportInfo> transportInfos;
 
   Impl(void* data, size_t size, int rank, TransportFlags transports, Communicator::Impl& commImpl);
+  /// Constructs a RegisteredMemory::Impl from a vector of data. The constructor should only be used for the remote
+  /// memory.
   Impl(const std::vector<char>& data);
+  ~Impl();
 
   const TransportInfo& getTransportInfo(Transport transport) const;
 };
