b6a876b722e46086caea6640197a97f924b895e5
Changho Hwang <changhohwang@microsoft.com>
2023-11-17 12:12:40 +0000

    Fix pylint

--- docs/conf.py
+++ docs/conf.py
@@ -6,24 +6,24 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'mscclpp'
-copyright = '2023, MSCCL++ Team'
-author = 'MSCCL++ Team'
-release = 'v0.2.0'
+project = "mscclpp"
+copyright = "2023, MSCCL++ Team"
+author = "MSCCL++ Team"
+release = "v0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['breathe']
+extensions = ["breathe"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # Breathe configuration
-breathe_default_project = 'mscclpp'
+breathe_default_project = "mscclpp"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
