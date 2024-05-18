b6d2a12881acc17ec71ce323d1dbee1ff4dd0ae9
Changho Hwang <changhohwang@microsoft.com>
2024-05-18 22:17:56 +0000

    Fix assert declaration & add a compile test

---
+++ test/unit/compile_tests.cu
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include <gtest/gtest.h>
+
+#include <mscclpp/poll_device.hpp>
+
+TEST(CompileTest, Assert) { assert(true); }
