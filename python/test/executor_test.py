bd34d9f2455a4662b772e47579071cd53c92f4e7
Binyang Li <binyli@microsoft.com>
2024-05-28 08:11:14 +0000

    add correctness check

--- python/test/executor_test.py
+++ python/test/executor_test.py
@@ -45,30 +45,40 @@ if __name__ == "__main__":
     cp.cuda.Device(MPI.COMM_WORLD.rank % mscclpp_group.nranks_per_node).use()
     executor = Executor(mscclpp_group.communicator)
     execution_plan = ExecutionPlan(
-        "allreduce_pairs", path.join(MSCCLPP_ROOT_PATH, "test", "execution-files", "allreduce.json")
+        "allreduce_mi300", path.join(MSCCLPP_ROOT_PATH, "test", "execution-files", "allreduce5.json")
     )
 
-    nelems = 1024 * 1024
+    nelems = 1024 * 1024 * 200
     cp.random.seed(42)
     buffer = cp.random.random(nelems).astype(cp.float16)
     sub_arrays = cp.split(buffer, MPI.COMM_WORLD.size)
     sendbuf = sub_arrays[MPI.COMM_WORLD.rank]
+    expected = cp.zeros_like(sendbuf)
+    for i in range(mscclpp_group.nranks):
+        expected += sub_arrays[i]
     mscclpp_group.barrier()
+
+    executor_func = lambda stream: executor.execute(
+        MPI.COMM_WORLD.rank,
+        sendbuf.data.ptr,
+        sendbuf.data.ptr,
+        sendbuf.nbytes,
+        sendbuf.nbytes,
+        DataType.float16,
+        512,
+        execution_plan,
+        stream.ptr,
+    )
+    # check correctness
+    stream = cp.cuda.Stream(non_blocking=True)
+    executor_func(stream)
+    stream.synchronize()
+    assert cp.allclose(sendbuf, expected, atol=1e-2 * mscclpp_group.nranks)
 
     execution_time = bench_time(
         100,
         10,
-        lambda stream: executor.execute(
-            MPI.COMM_WORLD.rank,
-            sendbuf.data.ptr,
-            sendbuf.data.ptr,
-            sendbuf.nbytes,
-            sendbuf.nbytes,
-            DataType.float16,
-            512,
-            execution_plan,
-            stream.ptr,
-        ),
+        executor_func
     )
     print(f"Rank: {MPI.COMM_WORLD.rank} Execution time: {execution_time} us, data size: {sendbuf.nbytes} bytes")
     executor = None
