66d82b83ab91df1b1ad4eba5419bb548585bc6f2
Caio Rocha <caiorocha@microsoft.com>
2024-09-11 08:59:43 +0000

    fix executor_test.py and expand test

--- python/test/executor_test.py
+++ python/test/executor_test.py
@@ -78,10 +78,11 @@ def dtype_to_mscclpp_dtype(dtype):
 
 
 def main(
-    execution_paln_name: str,
+    execution_plan_name: str,
     execution_plan_path: str,
     size: int,
     nthreads_per_block: int,
+    in_place: bool = True,
     dtype: cp.dtype = cp.float16,
     packet_type: PacketType = PacketType.LL16,
     seed: int = 42,
@@ -92,24 +93,38 @@ def main(
     npkit_dump_dir = os.getenv("NPKIT_DUMP_DIR")
     if npkit_dump_dir is not None:
         npkit.init(mscclpp_group.my_rank)
-    execution_plan = ExecutionPlan(execution_paln_name, execution_plan_path)
+    execution_plan = ExecutionPlan(execution_plan_name, execution_plan_path)
 
-    cp.random.seed(seed)
-    nelems = size // cp.dtype(dtype).itemsize
-    buffer = cp.random.random(nelems * mscclpp_group.nranks).astype(dtype)
-    sub_arrays = cp.split(buffer, MPI.COMM_WORLD.size)
-    sendbuf = sub_arrays[MPI.COMM_WORLD.rank]
-    expected = cp.zeros_like(sendbuf)
-    for i in range(mscclpp_group.nranks):
-        expected += sub_arrays[i]
+    if "allgather" in execution_plan_name:
+        cp.random.seed(seed)
+        nelems = size // cp.dtype(dtype).itemsize
+        buffer = cp.random.random(nelems * mscclpp_group.nranks).astype(dtype)
+        sub_arrays = cp.split(buffer, MPI.COMM_WORLD.size)
+        sendbuf = cp.zeros_like(sub_arrays[MPI.COMM_WORLD.rank])
+        for i in range(nelems):
+            sendbuf[i] = sub_arrays[MPI.COMM_WORLD.rank][i]
+        recvbuf = cp.zeros_like(buffer)
+        expected = buffer
+    else:
+        cp.random.seed(seed)
+        nelems = size // cp.dtype(dtype).itemsize
+        buffer = cp.random.random(nelems * mscclpp_group.nranks).astype(dtype)
+        sub_arrays = cp.split(buffer, MPI.COMM_WORLD.size)
+        sendbuf = cp.zeros_like(sub_arrays[MPI.COMM_WORLD.rank])
+        for i in range(nelems):
+            sendbuf[i] = sub_arrays[MPI.COMM_WORLD.rank][i]
+        recvbuf = cp.zeros_like(sendbuf)
+        expected = cp.zeros_like(sendbuf)
+        for i in range(mscclpp_group.nranks):
+            expected += sub_arrays[i]
     mscclpp_group.barrier()
 
     executor_func = lambda stream: executor.execute(
         MPI.COMM_WORLD.rank,
         sendbuf.data.ptr,
-        sendbuf.data.ptr,
+        sendbuf.data.ptr if in_place else recvbuf.data.ptr,
         sendbuf.nbytes,
-        sendbuf.nbytes,
+        sendbuf.nbytes if in_place else recvbuf.nbytes,
         dtype_to_mscclpp_dtype(dtype),
         nthreads_per_block,
         execution_plan,
@@ -120,7 +135,8 @@ def main(
     stream = cp.cuda.Stream(non_blocking=True)
     executor_func(stream)
     stream.synchronize()
-    assert cp.allclose(sendbuf, expected, atol=1e-2 * mscclpp_group.nranks)
+
+    assert cp.allclose(sendbuf if in_place else recvbuf, expected, atol=1e-2 * mscclpp_group.nranks)
 
     mscclpp_group.barrier()
     execution_time = bench_time(100, 10, executor_func)
@@ -142,6 +158,7 @@ if __name__ == "__main__":
     parser.add_argument("-path", "--execution_plan_path", type=str, required=True)
     parser.add_argument("--size", type=str, required=True)
     parser.add_argument("--nthreads_per_block", type=int, required=True)
+    parser.add_argument("--in_place", type=str, default="true", help="Choose from true, false")
     parser.add_argument("--dtype", type=str, default="float16", help="Choose from float16, float32, int32")
     parser.add_argument("--packet_type", type=str, default="LL16", help="Choose from LL8, LL16")
     parser.add_argument("--seed", type=int, default=42)
@@ -158,6 +175,7 @@ if __name__ == "__main__":
         args.execution_plan_path,
         buffer_size,
         args.nthreads_per_block,
+        args.in_place.lower() == "true",
         dtype,
         packet_type,
         args.seed,
