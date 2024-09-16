a8c30c0511f0aefa138e9fe41a38831d9a9ba776
Changho Hwang <changhohwang@microsoft.com>
2024-09-16 23:30:40 +0000

    clipping fp16/bf16 addition

--- apps/nccl/src/allreduce.hpp
+++ apps/nccl/src/allreduce.hpp
@@ -29,18 +29,51 @@ __forceinline__ __device__ To bit_cast(const From& src) {
 }
 
 template <typename T>
+__forceinline__ __device__ T clip(T val) {
+  return val;
+}
+
+template <>
+__forceinline__ __device__ __half clip(__half val) {
+  val = __hmax(val, bit_cast<__half, unsigned short>(0xfbff));
+  val = __hmin(val, bit_cast<__half, unsigned short>(0x7bff));
+  return val;
+}
+
+template <>
+__forceinline__ __device__ __half2 clip(__half2 val) {
+  val.x = clip(val.x);
+  val.y = clip(val.y);
+  return val;
+}
+
+template <>
+__forceinline__ __device__ __bfloat16 clip(__bfloat16 val) {
+  val = __hmax(val, bit_cast<__bfloat16, unsigned short>(0xff80));
+  val = __hmin(val, bit_cast<__bfloat16, unsigned short>(0x7f80));
+  return val;
+}
+
+template <>
+__forceinline__ __device__ __bfloat162 clip(__bfloat162 val) {
+  val.x = clip(val.x);
+  val.y = clip(val.y);
+  return val;
+}
+
+template <typename T>
 __forceinline__ __device__ T add_elements(T a, T b) {
-  return a + b;
+  return clip(a + b);
 }
 
 template <>
 __forceinline__ __device__ __half2 add_elements(__half2 a, __half2 b) {
-  return __hadd2(a, b);
+  return clip(__hadd2(a, b));
 }
 
 template <>
 __forceinline__ __device__ __bfloat162 add_elements(__bfloat162 a, __bfloat162 b) {
-  return __hadd2(a, b);
+  return clip(__hadd2(a, b));
 }
 
 template <typename T>
