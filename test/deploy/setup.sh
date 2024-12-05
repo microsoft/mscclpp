60a6e81e85dd7d0931097bed952d806af20a359d
Binyang Li <binyli@microsoft.com>
2024-12-05 17:39:31 +0000

    revert

--- test/deploy/setup.sh
+++ test/deploy/setup.sh
@@ -1,15 +1,12 @@
 set -e
 
-NODE_SCALE=$1
-if [ "${NODE_SCALE}" != "single" ]; then
-    mkdir -p /root/.ssh
-    mv /root/mscclpp/sshkey.pub /root/.ssh/authorized_keys
-    chown root:root /root/.ssh/authorized_keys
-    mv /root/mscclpp/test/deploy/config /root/.ssh/config
-    chown root:root /root/.ssh/config
-    chmod 400 /root/mscclpp/sshkey
-    chown root:root /root/mscclpp/sshkey
-fi
+mkdir -p /root/.ssh
+mv /root/mscclpp/sshkey.pub /root/.ssh/authorized_keys
+chown root:root /root/.ssh/authorized_keys
+mv /root/mscclpp/test/deploy/config /root/.ssh/config
+chown root:root /root/.ssh/config
+chmod 400 /root/mscclpp/sshkey
+chown root:root /root/mscclpp/sshkey
 
 nvidia-smi -pm 1
 for i in $(seq 0 $(( $(nvidia-smi -L | wc -l) - 1 ))); do
