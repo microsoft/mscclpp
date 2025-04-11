6c4eba40b106ce71ad37a13a8bf220c91dd5b34e
Binyang Li <binyli@microsoft.com>
2025-04-11 23:57:12 +0000

    fix

--- include/mscclpp/poll_device.hpp
+++ include/mscclpp/poll_device.hpp
@@ -13,7 +13,6 @@
 #if !defined(DEBUG_BUILD)
 #define __assert_fail(__assertion, __file, __line, __function) ;
 #else  // defined(DEBUG_BUILD)
-static_assert(sizeof(int) == 2, "int must be 4 bytes");
 #if defined(MSCCLPP_DEVICE_HIP)
 extern "C" __device__ void __assert_fail(const char *__assertion, const char *__file, unsigned int __line,
                                          const char *__function);
