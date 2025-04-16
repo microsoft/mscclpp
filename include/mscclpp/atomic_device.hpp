e412804eabcb1a4b5902952a97bc3032e8ee1b96
Binyang Li <binyli@microsoft.com>
2025-04-16 14:22:10 -0700

    Improve signal/wait performance and fix barrier issue (#499)
    Remove __assert_fail for release build. This will reduce the number of
    PTX instructions inside the loop. Also Trying to resolve this issue
    reported in #497. Reduce the number of PTX instructions from 8 to 6.
    8 ranks signal/wait will reduce from 3.2us->2.8us on NDv5
    Also NDEBUG flag is confused here, sometime it will not be set. Use
    customized flag for debug build.
    
    Here is current PTX:
    ```
          ld.u64  %rd12, [%rd2+-24];
          mov.u64         %rd13, %rd12;
          mov.u64         %rd11, %rd13;
          ld.acquire.sys.b64 %rd10,[%rd11];
          setp.lt.u64     %p1, %rd10, %rd3;
          @%p1 bra        $L__BB2_1;
    ```
    
    If we change to `asm volatile("ld.global.acquire.sys.b64 %0, [%1];" :
    "=l"(flag) : "l"(flag_addr));` will reduce to 4 instructions. We can get
    2.1 us for 8 ranks signal/wait
    ```
            ld.u64  %rd9, [%rd1+-24];
            ld.global.acquire.sys.b64 %rd8, [%rd9];
            setp.lt.u64     %p1, %rd8, %rd2;
            @%p1 bra        $L__BB2_1;
    ```

--- include/mscclpp/atomic_device.hpp
+++ include/mscclpp/atomic_device.hpp
@@ -20,19 +20,22 @@ constexpr cuda::memory_order memoryOrderRelease = cuda::memory_order_release;
 constexpr cuda::memory_order memoryOrderAcqRel = cuda::memory_order_acq_rel;
 constexpr cuda::memory_order memoryOrderSeqCst = cuda::memory_order_seq_cst;
 
-template <typename T>
+constexpr cuda::thread_scope scopeSystem = cuda::thread_scope_system;
+constexpr cuda::thread_scope scopeDevice = cuda::thread_scope_device;
+
+template <typename T, cuda::thread_scope Scope = cuda::thread_scope_system>
 MSCCLPP_HOST_DEVICE_INLINE T atomicLoad(T* ptr, cuda::memory_order memoryOrder) {
-  return cuda::atomic_ref<T, cuda::thread_scope_system>{*ptr}.load(memoryOrder);
+  return cuda::atomic_ref<T, Scope>{*ptr}.load(memoryOrder);
 }
 
-template <typename T>
+template <typename T, cuda::thread_scope Scope = cuda::thread_scope_system>
 MSCCLPP_HOST_DEVICE_INLINE void atomicStore(T* ptr, const T& val, cuda::memory_order memoryOrder) {
-  cuda::atomic_ref<T, cuda::thread_scope_system>{*ptr}.store(val, memoryOrder);
+  cuda::atomic_ref<T, Scope>{*ptr}.store(val, memoryOrder);
 }
 
-template <typename T>
+template <typename T, cuda::thread_scope Scope = cuda::thread_scope_system>
 MSCCLPP_HOST_DEVICE_INLINE T atomicFetchAdd(T* ptr, const T& val, cuda::memory_order memoryOrder) {
-  return cuda::atomic_ref<T, cuda::thread_scope_system>{*ptr}.fetch_add(val, memoryOrder);
+  return cuda::atomic_ref<T, Scope>{*ptr}.fetch_add(val, memoryOrder);
 }
 
 #elif defined(MSCCLPP_DEVICE_HIP)
@@ -43,17 +46,21 @@ constexpr auto memoryOrderRelease = __ATOMIC_RELEASE;
 constexpr auto memoryOrderAcqRel = __ATOMIC_ACQ_REL;
 constexpr auto memoryOrderSeqCst = __ATOMIC_SEQ_CST;
 
-template <typename T>
+// HIP does not have thread scope enums like CUDA
+constexpr auto scopeSystem = 0;
+constexpr auto scopeDevice = 0;
+
+template <typename T, int scope = scopeSystem>
 MSCCLPP_HOST_DEVICE_INLINE T atomicLoad(const T* ptr, int memoryOrder) {
   return __atomic_load_n(ptr, memoryOrder);
 }
 
-template <typename T>
+template <typename T, int scope = scopeSystem>
 MSCCLPP_HOST_DEVICE_INLINE void atomicStore(T* ptr, const T& val, int memoryOrder) {
   __atomic_store_n(ptr, val, memoryOrder);
 }
 
-template <typename T>
+template <typename T, int scope = scopeSystem>
 MSCCLPP_HOST_DEVICE_INLINE T atomicFetchAdd(T* ptr, const T& val, int memoryOrder) {
   return __atomic_fetch_add(ptr, val, memoryOrder);
 }
