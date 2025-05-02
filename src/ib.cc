81719dce83f7fcb3f63abd17105a1ebf55470f4e
Qinghua Zhou <qinghuazhou@microsoft.com>
2025-05-02 00:43:55 +0000

    Update code for ibv_reg_dmabuf_mr

--- src/ib.cc
+++ src/ib.cc
@@ -61,47 +61,34 @@ IbMr::IbMr(ibv_pd* pd, void* buff, std::size_t size) : buff(buff) {
 #endif  // !defined(__HIP_PLATFORM_AMD__)
   if (cuMemAlloc && dmaBufSupported) {
 #if !defined(__HIP_PLATFORM_AMD__)
-    CUdeviceptr base;
-    size_t actualSize;
-    MSCCLPP_CUTHROW(cuMemGetAddressRange(&base, &actualSize, dptr));
-
-    size_t offset = static_cast<size_t>(dptr - base);
-
-    // Align offset down to the nearest page
-    size_t alignedOffset = (offset / pageSize) * pageSize;
-
-    // Align userBufferSize up to the nearest page
-    size_t alignedUserBufferSize = ((size + pageSize - 1) / pageSize) * pageSize;
-
-    // Ensure aligned range fits within the original allocation
-    if (alignedOffset + alignedUserBufferSize > actualSize) {
-      std::stringstream err;
-      err << "aligned range excceeds original allocation (errno " << errno << ")";
-      throw mscclpp::IbError(err.str(), errno);
-    }
-
     int fd;
-    MSCCLPP_CUTHROW(cuMemGetHandleForAddressRange(&fd, base + alignedOffset, alignedUserBufferSize,
-                                                  CU_MEM_RANGE_HANDLE_TYPE_DMA_BUF_FD, 0));
+    MSCCLPP_CUTHROW(cuMemGetHandleForAddressRange(&fd, addr, pages * pageSize, CU_MEM_RANGE_HANDLE_TYPE_DMA_BUF_FD, 0));
 
-    // Compute offset of dptr within the DMA-BUF
-    size_t offsetInDmaBuf = offset - alignedOffset;
-
+    size_t offsetInDmaBuf = dptr % pageSize;
     this->mr = IBVerbs::ibv_reg_dmabuf_mr(pd, offsetInDmaBuf, size, (uint64_t)dptr, fd,
                                           IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ |
                                               IBV_ACCESS_RELAXED_ORDERING | IBV_ACCESS_REMOTE_ATOMIC);
+    if (this->mr == nullptr) {
+      std::stringstream err;
+      err << "ibv_reg_dmabuf_mr failed (errno " << errno << ")";
+      throw mscclpp::IbError(err.str(), errno);
+    }
+#else
+    std::stringstream err;
+    err << "Registeration of dma-buf based memory region failed on HIP platform (errno " << errno << ")";
+    throw mscclpp::IbError(err.str(), errno);
 #endif  // !defined(__HIP_PLATFORM_AMD__)
   } else {
     this->mr = IBVerbs::ibv_reg_mr2(pd, reinterpret_cast<void*>(addr), pages * pageSize,
                                     IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ |
                                         IBV_ACCESS_RELAXED_ORDERING | IBV_ACCESS_REMOTE_ATOMIC);
+    if (this->mr == nullptr) {
+      std::stringstream err;
+      err << "ibv_reg_mr failed (errno " << errno << ")";
+      throw mscclpp::IbError(err.str(), errno);
+    }
   }
 
-  if (this->mr == nullptr) {
-    std::stringstream err;
-    err << "ibv_reg_mr failed (errno " << errno << ")";
-    throw mscclpp::IbError(err.str(), errno);
-  }
   this->size = pages * pageSize;
 }
 
