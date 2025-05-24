f27e56bc329c6f9fde1a361d57f3d1345dd8b9a3
Changho Hwang <changhohwang@microsoft.com>
2025-05-24 07:27:27 +0000

    local port channel

--- src/include/communicator.hpp
+++ src/include/communicator.hpp
@@ -8,6 +8,7 @@
 #include <mscclpp/core.hpp>
 #include <unordered_map>
 #include <vector>
+#include <list>
 
 #include "utils_internal.hpp"
 
@@ -33,6 +34,23 @@ class RecvItem : public BaseRecvItem {
   std::shared_future<T> future_;
 };
 
+class LocalRecvMemory {
+ public:
+  LocalRecvMemory() : future_(std::move(promise_.get_future())) {}
+
+  void set(RegisteredMemory memory) {
+    promise_.set_value(std::move(memory));
+  }
+
+  std::shared_future<RegisteredMemory> reference() const { return future_; }
+
+  bool isReady() const { return future_.valid() && (future_.wait_for(std::chrono::seconds(0)) == std::future_status::ready); }
+
+ private:
+  std::promise<RegisteredMemory> promise_;
+  std::shared_future<RegisteredMemory> future_;
+};
+
 struct ConnectionInfo {
   int remoteRank;
   int tag;
@@ -42,11 +60,14 @@ struct Communicator::Impl {
   std::shared_ptr<Bootstrap> bootstrap_;
   std::shared_ptr<Context> context_;
   std::unordered_map<const Connection*, ConnectionInfo> connectionInfos_;
-  std::shared_ptr<BaseRecvItem> lastRecvItem_;
 
   // Temporary storage for the latest RecvItem of each {remoteRank, tag} pair.
   // If the RecvItem gets ready, it will be removed at the next call to getLastRecvItem.
   std::unordered_map<std::pair<int, int>, std::shared_ptr<BaseRecvItem>, PairHash> lastRecvItems_;
+
+  // RegisteredMemory items sent to the local rank of each tag. Sending memory to the local rank is
+  // unnecessary, but we enable this for consistency of the Communicator interface.
+  std::unordered_map<int, std::list<LocalRecvMemory>> localRecvMemories_;
 
   Impl(std::shared_ptr<Bootstrap> bootstrap, std::shared_ptr<Context> context);
 
---
+++ test/unit/local_channel_tests.cu
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include <gtest/gtest.h>
+
+#include <mscclpp/core.hpp>
+#include <mscclpp/gpu_utils.hpp>
+#include <mscclpp/port_channel.hpp>
+#include <mscclpp/port_channel_device.hpp>
+
+#define MAGIC_CONST 777
+
+__constant__ mscclpp::PortChannelDeviceHandle gPortChannel;
+
+__global__ void kernelLocalPortChannelTest(void *dst, void *src, size_t bytes, int* ret) {
+  if (blockIdx.x == 0) {
+    // sender
+    int *ptr = reinterpret_cast<int *>(src);
+    for (size_t idx = threadIdx.x; idx < bytes / sizeof(int); idx += blockDim.x) {
+      ptr[idx] = MAGIC_CONST;
+    }
+    __syncthreads();
+    if (threadIdx.x == 0) {
+      gPortChannel.putWithSignal(0, bytes);
+    }
+  } else if (blockIdx.x == 1) {
+    // receiver
+    if (threadIdx.x == 0) {
+      gPortChannel.wait();
+    }
+    __syncthreads();
+    int *ptr = reinterpret_cast<int *>(dst);
+    for (size_t idx = threadIdx.x; idx < bytes / sizeof(int); idx += blockDim.x) {
+      if (ptr[idx] != MAGIC_CONST) {
+        *ret = 1;  // Error: value mismatch
+        return;
+      }
+    }
+  }
+}
+
+static void localPortChannelTest(mscclpp::Transport transport) {
+  MSCCLPP_CUDATHROW(cudaSetDevice(0));
+
+  auto bootstrap = std::make_shared<mscclpp::TcpBootstrap>(/*rank*/0, /*nRanks*/1);
+  bootstrap->initialize(mscclpp::TcpBootstrap::createUniqueId());
+  auto communicator = std::make_shared<mscclpp::Communicator>(bootstrap);
+
+  auto connectionFuture = communicator->connect(/*remoteRank*/0, /*tag*/0, transport);
+
+  const size_t bytes = 4 * 1024 * 1024;
+  auto srcBuff = mscclpp::GpuBuffer(bytes).memory();
+  auto dstBuff = mscclpp::GpuBuffer(bytes).memory();
+
+  auto srcMem = communicator->registerMemory(srcBuff.get(), bytes, transport);
+  auto dstMem = communicator->registerMemory(dstBuff.get(), bytes, transport);
+
+  auto proxyService = std::make_shared<mscclpp::ProxyService>();
+  auto srcMemId = proxyService->addMemory(srcMem);
+  auto dstMemId = proxyService->addMemory(dstMem);
+
+  auto sid = proxyService->buildAndAddSemaphore(*communicator, connectionFuture.get());
+  auto portChannel = proxyService->portChannel(sid, dstMemId, srcMemId);
+  auto portChannelHandle = portChannel.deviceHandle();
+
+  MSCCLPP_CUDATHROW(cudaMemcpyToSymbol(gPortChannel, &portChannelHandle, sizeof(portChannelHandle)));
+
+  std::shared_ptr<int> ret = mscclpp::detail::gpuCallocHostShared<int>();
+
+  proxyService->startProxy();
+  kernelLocalPortChannelTest<<<2, 1024>>>(dstBuff.get(), srcBuff.get(), bytes, ret.get());
+  MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
+  proxyService->stopProxy();
+
+  EXPECT_EQ(*ret, 0);
+}
+
+TEST(LocalChannelTest, PortChannel) { localPortChannelTest(mscclpp::Transport::CudaIpc); }
