e05fed6f7e56d9a80d11f345ffebfbb489b47b90
Caio Rocha <caiorocha@microsoft.com>
2025-06-11 23:32:09 +0000

    wip

--- python/mscclpp/language/tests/unit_tests/run.sh
+++
@@ -1,9 +0,0 @@
-cd /root/mscclpp/python/mscclpp/language/tests/unit_tests
-for file in *.py; do
-  python3 "$file"
-  if [ $? -ne 0 ]; then
-    echo "❌ $file failed (exit code $?)"
-  else
-    echo "✅ $file succeeded"
-  fi
-done
