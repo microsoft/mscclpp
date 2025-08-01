ca2c320bb322db2c42bb9de2c32eca32519e9d02
Binyang Li <binyli@microsoft.com>
2025-08-01 20:23:30 +0000

    address comments

--- src/include/connection.hpp
+++ src/include/connection.hpp
@@ -38,7 +38,7 @@ class IBConnection : public Connection {
  private:
   Transport transport_;
   Transport remoteTransport_;
-  std::shared_ptr<IbQp> qp_;
+  std::weak_ptr<IbQp> qp_;
   std::unique_ptr<uint64_t> dummyAtomicSource_;  // not used anywhere but IB needs a source
   RegisteredMemory dummyAtomicSourceMem_;
   mscclpp::TransportInfo dstTransportInfo_;
