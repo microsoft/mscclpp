2eadbaf86ffb57b16e54c1725783a2ed5bc1791e
Changho Hwang <changhohwang@microsoft.com>
2025-08-11 10:34:29 -0700

    python doc auto generation (#605)
    Add Python API references

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,36 @@ release = "v0.7.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["breathe", "myst_parser", "sphinxcontrib.mermaid"]
+import os, sys
+
+sys.path.insert(0, os.path.abspath("../python"))
+
+extensions = [
+    "breathe",
+    "myst_parser",
+    "sphinxcontrib.mermaid",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx_autodoc_typehints",
+]
+
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "inherited-members": True,
+    "show-inheritance": True,
+}
+# only mock the C-extension when using the source tree
+autodoc_mock_imports = ["mscclpp._mscclpp", "sortedcontainers"]
+autodoc_typehints = "description"
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
