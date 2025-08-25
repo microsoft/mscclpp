547a9ae65cc1af9064c2951321f9afff6753feb8
Changho Hwang <changhohwang@microsoft.com>
2025-08-25 14:15:45 -0500

    Fixed cpp linter (#619)

--- src/semaphore.cc
+++ src/semaphore.cc
@@ -1,8 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-#include <mscclpp/semaphore.hpp>
 #include <mscclpp/gpu_utils.hpp>
+#include <mscclpp/semaphore.hpp>
 
 #include "api.h"
 #include "atomic.hpp"
