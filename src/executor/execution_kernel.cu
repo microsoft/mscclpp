10b453b4fd3754b9c679bc7afa47e9863dcf3e8d
Binyang Li <root@mscclpp-h100000001.rxefiojanttepawbuxfs5dqovc.gvxx.internal.cloudapp.net>
2025-10-09 20:32:05 +0000

    fix hang issue

--- src/executor/execution_kernel.cu
+++ src/executor/execution_kernel.cu
@@ -8,13 +8,13 @@ namespace mscclpp {
 
 template <typename PacketType, bool ReuseScratch>
 void ExecutionKernel::launchKernel(int rank, int nthreadblocks, int nthreads, void* src, void* dst, void* scratch,
-                                   uint32_t scratchOffset, uint32_t scrachChunkSize, DataType dataType,
+                                   uint32_t scratchOffset, uint32_t scratchChunkSize, DataType dataType,
                                    DeviceExecutionPlan* plan, DeviceSemaphore* semaphores, uint32_t sharedMemSize,
                                    cudaStream_t stream, uint32_t flag) {
   switch (dataType) {
     case DataType::INT32:
       executionKernel<int32_t, PacketType, ReuseScratch><<<nthreadblocks, nthreads, sharedMemSize, stream>>>(
-          rank, (int32_t*)src, (int32_t*)dst, (int32_t*)scratch, scratchOffset, scrachChunkSize, plan, semaphores, flag
+          rank, (int32_t*)src, (int32_t*)dst, (int32_t*)scratch, plan, semaphores, scratchOffset, scratchChunkSize, flag
 #if defined(ENABLE_NPKIT)
           ,
           NpKit::GetGpuEventCollectContexts(), NpKit::GetCpuTimestamp());
@@ -24,7 +24,7 @@ void ExecutionKernel::launchKernel(int rank, int nthreadblocks, int nthreads, vo
       break;
     case DataType::UINT32:
       executionKernel<uint32_t, PacketType, ReuseScratch><<<nthreadblocks, nthreads, sharedMemSize, stream>>>(
-          rank, (uint32_t*)src, (uint32_t*)dst, (uint32_t*)scratch, scratchOffset, scrachChunkSize, plan, semaphores,
+          rank, (uint32_t*)src, (uint32_t*)dst, (uint32_t*)scratch, plan, semaphores, scratchOffset, scratchChunkSize,
           flag
 #if defined(ENABLE_NPKIT)
           ,
@@ -35,7 +35,7 @@ void ExecutionKernel::launchKernel(int rank, int nthreadblocks, int nthreads, vo
       break;
     case DataType::FLOAT16:
       executionKernel<half, PacketType, ReuseScratch><<<nthreadblocks, nthreads, sharedMemSize, stream>>>(
-          rank, (half*)src, (half*)dst, (half*)scratch, scratchOffset, scrachChunkSize, plan, semaphores, flag
+          rank, (half*)src, (half*)dst, (half*)scratch, plan, semaphores, scratchOffset, scratchChunkSize, flag
 #if defined(ENABLE_NPKIT)
           ,
           NpKit::GetGpuEventCollectContexts(), NpKit::GetCpuTimestamp());
@@ -45,7 +45,7 @@ void ExecutionKernel::launchKernel(int rank, int nthreadblocks, int nthreads, vo
       break;
     case DataType::FLOAT32:
       executionKernel<float, PacketType, ReuseScratch><<<nthreadblocks, nthreads, sharedMemSize, stream>>>(
-          rank, (float*)src, (float*)dst, (float*)scratch, scratchOffset, scrachChunkSize, plan, semaphores, flag
+          rank, (float*)src, (float*)dst, (float*)scratch, plan, semaphores, scratchOffset, scratchChunkSize, flag
 #if defined(ENABLE_NPKIT)
           ,
           NpKit::GetGpuEventCollectContexts(), NpKit::GetCpuTimestamp());
@@ -55,8 +55,8 @@ void ExecutionKernel::launchKernel(int rank, int nthreadblocks, int nthreads, vo
       break;
     case DataType::BFLOAT16:
       executionKernel<__bfloat16, PacketType, ReuseScratch><<<nthreadblocks, nthreads, sharedMemSize, stream>>>(
-          rank, (__bfloat16*)src, (__bfloat16*)dst, (__bfloat16*)scratch, scratchOffset, scrachChunkSize, plan,
-          semaphores, flag
+          rank, (__bfloat16*)src, (__bfloat16*)dst, (__bfloat16*)scratch, plan, semaphores, scratchOffset,
+          scratchChunkSize, flag
 #if defined(ENABLE_NPKIT)
           ,
           NpKit::GetGpuEventCollectContexts(), NpKit::GetCpuTimestamp());
