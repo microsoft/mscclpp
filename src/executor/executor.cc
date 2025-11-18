eb80f35276c10c8fb3cd238d2c9294777aaa6908
Caio Rocha <caiorocha@microsoft.com>
2025-11-18 00:26:35 +0000

    wip

--- src/executor/executor.cc
+++ src/executor/executor.cc
@@ -99,13 +99,7 @@ struct hash<mscclpp::DeviceExecutionPlanKey> {
 }  // namespace std
 
 namespace {
-auto hasIBDevices = []() {
-#if defined(USE_IBVERBS)
-  return mscclpp::getIBDeviceCount() > 0;
-#else
-  return false;
-#endif
-};
+auto hasIBDevices = []() { return mscclpp::getIBDeviceCount() > 0; };
 
 auto useIB = [](int rank1, int rank2, int nranksPerNode) {
   bool inSameNode = rank1 / nranksPerNode == rank2 / nranksPerNode;
