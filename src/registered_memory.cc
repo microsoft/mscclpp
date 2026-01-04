ca6a4a32740e5fafa4be152ae5dd97d331650d27
Binyang Li <binyli@microsoft.com>
2026-01-04 20:47:58 +0800

    Replace `__HIP_PLATFORM_AMD__` to use internal macro (#712)
    Replacing most of checks for `__HIP_PLATFORM_AMD__` with
    `MSCCLPP_DEVICE_HIP` for device and `MSCCLPP_USE_ROCM` for host source
    file.

--- src/registered_memory.cc
+++ src/registered_memory.cc
@@ -68,7 +68,7 @@ std::shared_ptr<void> getPeerMemoryHandle(cudaIpcMemHandle_t ipcHandle) {
       INFO(mscclpp::P2P, "Closed CUDA IPC handle at pointer ", std::hex, p);
     }
   };
-#if defined(__HIP_PLATFORM_AMD__)
+#if defined(MSCCLPP_USE_ROCM)
   // Unlike Nvidia, ROCm will not reuse the same ipc handle for same memory region.
   // We cache the opened ipc handles to avoid opening multiple times. (May exceed system limit on vm.max_map_count)
   static auto peerMemoryHandleMap = std::make_shared<
