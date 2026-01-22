64a2d4aaf84ed04d5c717d9039ba4c46f2a44ad2
mahdiehghazim <mahdiehghazi@microsoft.com>
2026-01-22 14:51:42 -0500

    clang formatted fix

--- include/mscclpp/switch_channel_device.hpp
+++ include/mscclpp/switch_channel_device.hpp
@@ -85,7 +85,7 @@ struct SwitchChannelDeviceHandle {
     } else if constexpr (std::is_same_v<VectorType, fp8_e4m3x8>) {
       asm("multimem.ld_reduce.relaxed.sys.global.add.v2.e4m3x4 {%0,%1}, [%2];" 
 		  : "=r"(val.words[0]), "=r"(val.words[1])
-		  : "l"(ptr) 
+		  : "l"(ptr)
 		  : "memory");
     } else if constexpr (std::is_same_v<VectorType, fp8_e4m3x16>) {
       asm("multimem.ld_reduce.relaxed.sys.global.add.v4.e4m3x4 {%0,%1,%2,%3}, [%4];"
@@ -148,7 +148,7 @@ struct SwitchChannelDeviceHandle {
       asm volatile("multimem.st.relaxed.sys.global.v4.bf16x2 [%0], {%1,%2,%3,%4};" ::"l"(ptr), "r"(val.words[0]),
                    "r"(val.words[1]), "r"(val.words[2]), "r"(val.words[3])
                    : "memory");
-    } else if constexpr (std::is_same_v<VectorType, fp8_e4m3x4>) { 
+    } else if constexpr (std::is_same_v<VectorType, fp8_e4m3x4>) {
       asm volatile("multimem.st.relaxed.sys.global.e4m3x4 [%0], %1;" ::"l"(ptr), "r"(val.words[0]) : "memory");
     } else if constexpr (std::is_same_v<VectorType, fp8_e4m3x8>) {
       asm volatile("multimem.st.relaxed.sys.global.v2.e4m3x4  [%0], {%1,%2};" ::"l"(ptr), "r"(val.words[0]),
