2b4adcc4ad42e5b7c74723ddf3db87b0d3915265
Changho Hwang <changhohwang@microsoft.com>
2026-02-18 20:33:57 -0800

    fix lint

--- test/executor_test.cc
+++ test/executor_test.cc
@@ -93,8 +93,8 @@ double benchTime(int rank, std::shared_ptr<mscclpp::Bootstrap> bootstrap, std::s
 
 int main(int argc, char* argv[]) {
   if (argc != 5 && argc != 6) {
-    std::cerr << "Usage: " << argv[0] << " <buffer size>" << " <execution plan path>" << " <number of iterations>"
-              << " <number of graph iterations>" << " (optional) <packet type>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <buffer size> <execution plan path>"
+              << " <number of iterations> <number of graph iterations> (optional) <packet type>" << std::endl;
     return 1;
   }
 
