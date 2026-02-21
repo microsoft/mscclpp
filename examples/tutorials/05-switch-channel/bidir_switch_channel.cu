76513d790c4934170444afb0455d4d10257595ca
Ubuntu <mahdiehghazi@microsoft.com>
2026-02-21 03:22:31 +0000

    lint

--- examples/tutorials/05-switch-channel/bidir_switch_channel.cu
+++ examples/tutorials/05-switch-channel/bidir_switch_channel.cu
@@ -45,7 +45,6 @@ int wait_process(int pid) {
   return -1;
 }
 
-
 __constant__ mscclpp::SwitchChannelDeviceHandle gConstSwitchChan;
 
 __device__ mscclpp::DeviceSyncer devSyncer;
@@ -54,16 +53,15 @@ __global__ void kernelSwitchReduce(int rank, int numElements) {
   const int tid = threadIdx.x + blockIdx.x * blockDim.x;
   int stride = blockDim.x * gridDim.x;
 
-  //rank0: 0-(numElements/2)-1
-  //rank1: (numElements/2)-(numElements-1)
-  int min=rank*(numElements/2);
-  int max=(rank+1)*(numElements/2);
+  // rank0: 0-(numElements/2)-1
+  // rank1: (numElements/2)-(numElements-1)
+  int min = rank * (numElements / 2);
+  int max = (rank + 1) * (numElements / 2);
 
-  for(int i=tid+min;i<max;i+=stride) {
-	  auto val = gConstSwitchChan.reduce<mscclpp::f32x1>(i);
-	  gConstSwitchChan.broadcast(i, val);
+  for (int i = tid + min; i < max; i += stride) {
+    auto val = gConstSwitchChan.reduce<mscclpp::f32x1>(i);
+    gConstSwitchChan.broadcast(i, val);
   }
-
 }
 
 int worker(int myRank, int gpuId, const std::string &ipPort) {
@@ -96,50 +94,56 @@ int worker(int myRank, int gpuId, const std::string &ipPort) {
 
   // Call the kernel in a loop for perf evaluation
 
-  for (size_t numElements : {1024 , 1024*1024, 32*1024*1024}) {
-  cudaEvent_t start, end;
-  if (myRank == 0) {
-    MSCCLPP_CUDATHROW(cudaEventCreate(&start));
-    MSCCLPP_CUDATHROW(cudaEventCreate(&end));
+  for (size_t numElements : {1024, 1024 * 1024, 32 * 1024 * 1024}) {
+    cudaEvent_t start, end;
+    if (myRank == 0) {
+      MSCCLPP_CUDATHROW(cudaEventCreate(&start));
+      MSCCLPP_CUDATHROW(cudaEventCreate(&end));
+    }
+    MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
+    bootstrap->barrier();
+
+    if (myRank == 0) {
+      MSCCLPP_CUDATHROW(cudaEventRecord(start, 0));
+    }
+
+    for (int i = 0; i < iter; ++i) {
+      kernelSwitchReduce<<<256, 1024>>>(myRank, numElements);
+    }
+
+    MSCCLPP_CUDATHROW(cudaGetLastError());
+    MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
+
+    comm->bootstrap()->barrier();
+
+    if (myRank == 0) {
+      MSCCLPP_CUDATHROW(cudaEventRecord(end, 0));
+      MSCCLPP_CUDATHROW(cudaEventSynchronize(end));
+      float elapsedTime;
+      float elapsedTimePerIter;
+      float gbps;
+      MSCCLPP_CUDATHROW(cudaEventElapsedTime(&elapsedTime, start, end));
+      elapsedTimePerIter = elapsedTime / iter;
+      float dataSize = numElements * 4;
+      gbps = dataSize / elapsedTimePerIter * 1e-6f;
+      log("Rank ", myRank, " (GPU ", gpuId, "): bytes ", dataSize, ", elapsed ", elapsedTimePerIter, " ms/iter, BW ",
+          gbps, " GB/s");
+    }
   }
-  MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
-  bootstrap->barrier();
 
-  if (myRank == 0) {
-        MSCCLPP_CUDATHROW(cudaEventRecord(start, 0));
-      }
-
-  for (int i = 0; i < iter; ++i) {
-  kernelSwitchReduce<<<256, 1024>>>(myRank,numElements);
-  }
-
-  MSCCLPP_CUDATHROW(cudaGetLastError());
-  MSCCLPP_CUDATHROW(cudaDeviceSynchronize());
-
-  comm->bootstrap()->barrier();
-
-   if (myRank == 0) {
-        MSCCLPP_CUDATHROW(cudaEventRecord(end,0));
-        MSCCLPP_CUDATHROW(cudaEventSynchronize(end));
-        float elapsedTime;
-        float elapsedTimePerIter;
-        float gbps;
-        MSCCLPP_CUDATHROW(cudaEventElapsedTime(&elapsedTime, start, end));
-        elapsedTimePerIter = elapsedTime / iter;
-	float dataSize = numElements*4;
-        gbps = dataSize / elapsedTimePerIter * 1e-6f;
-        log("Rank ", myRank, " (GPU ", gpuId, "): bytes ", dataSize, ", elapsed ", elapsedTimePerIter,
-            " ms/iter, BW ", gbps, " GB/s");
-      }
-  }
-
-	  return 0;
+  return 0;
 }
 
 int main(int argc, char **argv) {
   if (argc == 1) {
-    int pid0 = spawn_process([]() { int rc = worker(0, 0, "lo:127.0.0.1:" PORT_NUMBER); exit(rc); });
-    int pid1 = spawn_process([]() { int rc = worker(1, 1, "lo:127.0.0.1:" PORT_NUMBER); exit(rc); });
+    int pid0 = spawn_process([]() {
+      int rc = worker(0, 0, "lo:127.0.0.1:" PORT_NUMBER);
+      exit(rc);
+    });
+    int pid1 = spawn_process([]() {
+      int rc = worker(1, 1, "lo:127.0.0.1:" PORT_NUMBER);
+      exit(rc);
+    });
     if (pid0 < 0 || pid1 < 0) {
       log("Failed to spawn processes.");
       return -1;
@@ -170,11 +174,11 @@ int main(int argc, char **argv) {
       log("Error: rank must be between 0 and 1 and gpu_id must be non-negative.");
       return -1;
     }
-    if (worker(rank, gpuId, ipPort)==0) {
-    log("Rank ", rank, ": Succeed!");
-    return 0;
+    if (worker(rank, gpuId, ipPort) == 0) {
+      log("Rank ", rank, ": Succeed!");
+      return 0;
     } else {
-    return -1;
+      return -1;
     }
   } else {
     std::cerr << "Usage:\n"
