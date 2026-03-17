a191f16b76dbb0b27b94484fd468a741f9f73e5b
Ubuntu <mahdiehghazi@microsoft.com>
2026-03-17 20:06:15 +0000

    add scripts

---
+++ generate-json.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+# Check if the number of arguments is exactly 1
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <hostfile> <nnodes> <ppn>"
+    exit 1
+fi
+
+HOSTFILE=$1
+NNODES=$2
+PPN=$3
+
+parallel-scp -h "$HOSTFILE" -p32 -t1800 -r python/test/executor_test.py /home/azhpcuser/mahdieh/mscclpp/python/test/
+
+parallel-scp -h "$HOSTFILE" -p32 -t1800 -r python/mscclpp/default_algos/mscclpp_send_recv.py /home/azhpcuser/mahdieh/mscclpp/python/mscclpp/default_algos/ 
+
+parallel-ssh -h "$HOSTFILE" -p32 -i -t1800 "cd /home/azhpcuser/mahdieh/mscclpp && source mscclpp/bin/activate && python3 python/mscclpp/default_algos/mscclpp_send_recv.py --name send_recv_test --nnodes $NNODES --gpus_per_node $PPN --split_mask 0x3 > test.json "
