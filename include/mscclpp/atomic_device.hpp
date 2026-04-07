9a740274b383e02f060f54208c65ab5c5beb8d82
Binyang Li <binyli@microsoft.com>
2026-04-07 21:57:56 +0000

    WIP

--- include/mscclpp/atomic_device.hpp
+++ include/mscclpp/atomic_device.hpp
@@ -6,8 +6,6 @@
 
 #include "device.hpp"
 
-#include <type_traits>
-
 #if defined(MSCCLPP_DEVICE_CUDA)
 #include <cuda/atomic>
 #endif  // defined(MSCCLPP_DEVICE_CUDA)
@@ -40,20 +38,6 @@ MSCCLPP_HOST_DEVICE_INLINE T atomicFetchAdd(T* ptr, const T& val, cuda::memory_o
   return cuda::atomic_ref<T, Scope>{*ptr}.fetch_add(val, memoryOrder);
 }
 
-/// Fire-and-forget atomic add using PTX `red` (reduce) — no return value, more efficient than `atom`.
-#if defined(MSCCLPP_DEVICE_COMPILE)
-template <typename T, cuda::thread_scope Scope = cuda::thread_scope_system>
-MSCCLPP_DEVICE_INLINE void atomicAdd(T* ptr, const T& val, cuda::memory_order memoryOrder) {
-  static_assert(std::is_same_v<T, uint64_t>, "atomicAdd (red) only supports uint64_t");
-  static_assert(Scope == cuda::thread_scope_system, "atomicAdd (red) only supports system scope");
-  if (memoryOrder == cuda::memory_order_relaxed) {
-    asm volatile("red.relaxed.sys.global.add.u64 [%0], %1;" ::"l"(ptr), "l"(val) : "memory");
-  } else {
-    asm volatile("red.release.sys.global.add.u64 [%0], %1;" ::"l"(ptr), "l"(val) : "memory");
-  }
-}
-#endif  // defined(MSCCLPP_DEVICE_COMPILE)
-
 #elif defined(MSCCLPP_DEVICE_HIP)
 
 constexpr auto memoryOrderRelaxed = __ATOMIC_RELAXED;
@@ -80,14 +64,6 @@ template <typename T, int scope = scopeSystem>
 MSCCLPP_HOST_DEVICE_INLINE T atomicFetchAdd(T* ptr, const T& val, int memoryOrder) {
   return __atomic_fetch_add(ptr, val, memoryOrder);
 }
-
-/// Fire-and-forget atomic add — discards return value.
-#if defined(MSCCLPP_DEVICE_COMPILE)
-template <typename T, int scope = scopeSystem>
-MSCCLPP_DEVICE_INLINE void atomicAdd(T* ptr, const T& val, int memoryOrder) {
-  (void)__atomic_fetch_add(ptr, val, memoryOrder);
-}
-#endif  // defined(MSCCLPP_DEVICE_COMPILE)
 
 #endif  // defined(MSCCLPP_DEVICE_HIP)
 
