406cfcbe4e7036f03464ce34af43447ab6c6f02d
Caio Rocha <caiorocha@microsof.com>
2026-05-12 00:36:39 +0000

    wip

--- python/mscclpp/default_algos/__init__.py
+++ python/mscclpp/default_algos/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from mscclpp.default_algos.allreduce_2nodes import allreduce_2nodes
 from mscclpp.default_algos.allreduce_multi_nodes import allreduce_multi_nodes
 
-__all__ = ["allreduce_2nodes", "allreduce_multi_nodes"]
+__all__ = ["allreduce_multi_nodes"]
